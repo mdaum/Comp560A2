037186432e2d80e24168a15720ba1323168ea929
unknown <daumisgod@gmail.com>
2015-09-21 21:41:07 -0400

    added my ghost parser class

--- src/main/Runner.java
+++ src/main/Runner.java
@@ -3,7 +3,7 @@ package main;
 public class Runner {
 
 	public static void main(String[] args) { //imagine we can condition on which problem they do and pipe in corresponding parsing....
-		
+		HideSeekParser MaxWritesThis = new HideSeekParser();
 
 	}
 
