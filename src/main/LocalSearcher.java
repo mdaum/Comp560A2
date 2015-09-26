b712aede012ef086d7a190d2c2271d39c270c14d
unknown <daumisgod@gmail.com>
2015-09-25 23:54:13 -0400

    max first min first there

--- src/main/LocalSearcher.java
+++ src/main/LocalSearcher.java
@@ -12,8 +12,8 @@ public class LocalSearcher {
 		this.friends=friends;
 		this.conflictF=new ArrayList<forestSquare>();
 	}
-	public static void search(){
-		Random R = new Random();
+	public static void searchMin(){
+		int runs=0;
 		for (forestSquare friend : friends) {
 			int c = conflicts(friend);
 			if(c>0)conflictF.add(friend);
@@ -23,7 +23,19 @@ public class LocalSearcher {
 		int bestConflictCount = Integer.MAX_VALUE;
 		int bestCol=0;
 		while(!conflictF.isEmpty()){
-			chosenOne = conflictF.remove(R.nextInt(conflictF.size()));
+			chosenOne=null;
+			int bestCon=Integer.MAX_VALUE;
+			for (forestSquare doody : conflictF) {
+				int c =conflicts(doody);
+				if(c<bestCon){
+					chosenOne=doody;
+					bestCon=c;
+				}
+			}
+			runs++;
+			System.out.println();
+			Runner.printGrid(board);
+			conflictF.remove(chosenOne);
 			chosenOne.value='E';//to make sure that this friend doesn't conflict with itself
 			for(int i=0;i<board.length;i++){
 				if(board[chosenOne.row][i].value=='T')
@@ -35,9 +47,54 @@ public class LocalSearcher {
 				}
 			}
 			board[chosenOne.row][bestCol].value='F';
-			System.out.println();
-			System.out.println();
-			printGrid(board);
+			if(bestConflictCount!=0){
+				conflictF.add(board[chosenOne.row][bestCol]);
+				ArrayList<forestSquare> problemFriends = conflictingFriends(board[chosenOne.row][bestCol]);
+				for(forestSquare conflictingFriend : problemFriends){
+					if(!conflictF.contains(conflictingFriend))
+						conflictF.add(conflictingFriend);
+				}
+			}
+			//RESET DESE PROBLEM CHILLUNS
+			bestCol=0;
+			bestConflictCount=Integer.MAX_VALUE;
+		}
+		System.out.println("Finished after moving "+runs+" friends.");
+		//we're done
+	}
+	public static void searchMax(){
+		int runs=0;
+		for (forestSquare friend : friends) {
+			int c = conflicts(friend);
+			if(c>0)conflictF.add(friend);
+		}
+		forestSquare chosenOne;
+		int tempConflictCount;
+		int bestConflictCount = Integer.MAX_VALUE;
+		int bestCol=0;
+		while(!conflictF.isEmpty()){
+			chosenOne=null;
+			int bestCon=Integer.MIN_VALUE;
+			for (forestSquare doody : conflictF) {
+				int c =conflicts(doody);
+				if(c>bestCon){
+					chosenOne=doody;
+					bestCon=c;
+				}
+			}
+			runs++;
+			conflictF.remove(chosenOne);
+			chosenOne.value='E';//to make sure that this friend doesn't conflict with itself
+			for(int i=0;i<board.length;i++){
+				if(board[chosenOne.row][i].value=='T')
+					continue;
+				tempConflictCount=conflicts(board[chosenOne.row][i]);
+				if(tempConflictCount<bestConflictCount){
+					bestCol=i;
+					bestConflictCount=tempConflictCount;
+				}
+			}
+			board[chosenOne.row][bestCol].value='F';
 			if(bestConflictCount!=0){
 				conflictF.add(board[chosenOne.row][bestCol]);
 				ArrayList<forestSquare> problemFriends = conflictingFriends(board[chosenOne.row][bestCol]);
@@ -51,6 +108,47 @@ public class LocalSearcher {
 			bestConflictCount=Integer.MAX_VALUE;
 		}
 		//we're done
+		System.out.println("Finished after moving "+runs+" friends.");
+	}
+	public static void searchRand(){
+		int runs=0;
+		Random R = new Random();
+		for (forestSquare friend : friends) {
+			int c = conflicts(friend);
+			if(c>0)conflictF.add(friend);
+		}
+		forestSquare chosenOne;
+		int tempConflictCount;
+		int bestConflictCount = Integer.MAX_VALUE;
+		int bestCol=0;
+		while(!conflictF.isEmpty()){
+			chosenOne = conflictF.remove(R.nextInt(conflictF.size()));
+			runs++;
+			chosenOne.value='E';//to make sure that this friend doesn't conflict with itself
+			for(int i=0;i<board.length;i++){
+				if(board[chosenOne.row][i].value=='T')
+					continue;
+				tempConflictCount=conflicts(board[chosenOne.row][i]);
+				if(tempConflictCount<bestConflictCount){
+					bestCol=i;
+					bestConflictCount=tempConflictCount;
+				}
+			}
+			board[chosenOne.row][bestCol].value='F';
+			if(bestConflictCount!=0){
+				conflictF.add(board[chosenOne.row][bestCol]);
+				ArrayList<forestSquare> problemFriends = conflictingFriends(board[chosenOne.row][bestCol]);
+				for(forestSquare conflictingFriend : problemFriends){
+					if(!conflictF.contains(conflictingFriend))
+						conflictF.add(conflictingFriend);
+				}
+			}
+			//RESET DESE PROBLEM CHILLUNS
+			bestCol=0;
+			bestConflictCount=Integer.MAX_VALUE;
+		}
+		//we're done
+		System.out.println("Finished after moving "+runs+" friends.");
 	}
 	public static ArrayList<forestSquare> conflictingFriends(forestSquare square){
 		ArrayList<forestSquare> problemFriends = new ArrayList<forestSquare>();
@@ -68,7 +166,7 @@ public class LocalSearcher {
 		//check up+down
 		problemFriends.addAll(conflictingFriendsInDirection(square,0,-1));
 		problemFriends.addAll(conflictingFriendsInDirection(square,0,1));
-		System.out.println(square.row+","+square.col+" has "+problemFriends.size()+" conflicts");
+		//System.out.println(square.row+","+square.col+" has "+problemFriends.size()+" conflicts");
 		return problemFriends;
 	}
 	public static ArrayList<forestSquare> conflictingFriendsInDirection(forestSquare square, int xDirection, int yDirection){
@@ -101,7 +199,7 @@ public class LocalSearcher {
 		//check up+down
 		conflictCount+=conflictsInDirection(square,0,-1);
 		conflictCount+=conflictsInDirection(square,0,1);
-		System.out.println(square.row+","+square.col+" has "+conflictCount+" conflicts");
+		//System.out.println(square.row+","+square.col+" has "+conflictCount+" conflicts");
 		return conflictCount;
 	}
 	public static int conflictsInDirection(forestSquare square, int xDirection, int yDirection){
@@ -119,15 +217,4 @@ public class LocalSearcher {
 		return conflictCount;
 	}
 	
-	public static void printGrid(forestSquare[][] b)
-	{
-	   for(int i = 0; i < b.length; i++)
-	   {
-	      for(int j = 0; j < b[0].length; j++)
-	      {
-	         System.out.print(b[i][j].value);
-	      }
-	      System.out.println();
-	   }
-	}
 }
--- src/main/Runner.java
+++ src/main/Runner.java
@@ -10,16 +10,34 @@ public class Runner {
 		System.out.println("made board");
 		friends=new forestSquare[board.length];
 		printGrid(board);
+		System.out.println();
+		System.out.println();
 		System.out.println("randomly placing friends");
 		randomlyInsertFriends(board);
-		System.out.println();
-		System.out.println();
-		System.out.println();
+		forestSquare[][] board2=deepclone(board);
+		forestSquare[][] board3 =deepclone(board);
+		forestSquare[] friends2 = deepFriends(friends);
+		forestSquare[] friends3 = deepFriends(friends);
 		printGrid(board);
+		System.out.println();
+		System.out.println("Choosing Friends to move in random manner");
+		System.out.println();
 		LocalSearcher L= new LocalSearcher(board,friends);
-		L.search();
-		System.out.println("here is the new board!");
+		L.searchRand();
 		printGrid(L.board);
+		System.out.println();
+		System.out.println("Choosing Friends to move in min-conflict-first manner");
+		System.out.println();
+		LocalSearcher L2=new LocalSearcher(board2,friends2);
+		L2.searchMin();
+		printGrid(L2.board);
+		System.out.println();
+		System.out.println("Choosing Friends to move in max-conflict-first manner");
+		System.out.println();
+		LocalSearcher L3=new LocalSearcher(board3,friends3);
+		L3.searchMin();
+		printGrid(L3.board);
+		
 		
 	}
 	//RUNS THE RISK OF SMASHING INTO A TREE FOREVER
@@ -31,7 +49,6 @@ public class Runner {
 					Random R= new Random();
 					int col=R.nextInt(forestSquares.length-1);
 					if(forest[row][col].value=='E'){
-						System.out.println("found one!");
 						forest[row][col].value='F';
 						friends[row]=forest[row][col];
 						break;
@@ -50,5 +67,21 @@ public class Runner {
 	      }
 	      System.out.println();
 	   }
+	}
+	public static forestSquare[][] deepclone(forestSquare[][] orig){
+		forestSquare[][] returnable = new forestSquare[orig.length][orig.length];
+		for(int i=0;i<orig.length;i++){
+			for(int j=0;j<orig.length;j++){
+				returnable[i][j]=new forestSquare(orig[i][j].row,orig[i][j].col,orig[i][j].value);
+			}
+		}
+		return returnable;
+	}
+	public static forestSquare[] deepFriends(forestSquare[] orig){
+		forestSquare[] returnable = new forestSquare[orig.length];
+		for(int i=0;i<orig.length;i++){
+			returnable[i]=new forestSquare(orig[i].row,orig[i].col,orig[i].value);
+		}
+		return returnable;
 	}
 }
