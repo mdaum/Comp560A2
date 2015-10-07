111ceb078d953a27c28a450e9dc288837830f83f
Fred Landis <fcl@unc.edu>
2015-10-07 17:27:49 -0400

    fleshed out local search
    2 more forests added, Runner has cleaner print statements and a method
    to print out the assignment of friends, which is what we ACTUALLY
    technically need instead of a nice visual grid (I kept those)

--- src/main/Runner.java
+++ src/main/Runner.java
@@ -6,13 +6,13 @@ public class Runner {
 	static forestSquare[] friends;
 	public static void main(String[] args) { //imagine we can condition on which problem they do and pipe in corresponding parsing....
 		HideSeekParser MaxWritesThis = new HideSeekParser();
-		forestSquare[][] board = MaxWritesThis.makeBoard("sampleForest.txt");
-		System.out.println("made board");
+		forestSquare[][] board = MaxWritesThis.makeBoard("sampleForest3.txt");
+		//System.out.println("made board");
 		friends=new forestSquare[board.length];
-		printGrid(board);
-		System.out.println();
-		System.out.println();
-		System.out.println("randomly placing friends");
+		//printGrid(board);
+		//System.out.println();
+		//System.out.println();
+		System.out.println("Randomly placing friends:");
 		randomlyInsertFriends(board);
 		forestSquare[][] board2=deepclone(board);
 		forestSquare[][] board3 =deepclone(board);
@@ -24,29 +24,33 @@ public class Runner {
 			friends3[i]=board3[friends[i].row][friends[i].col];
 		}
 		printGrid(board);
+		printFriends(board);
 		System.out.println();
 		System.out.println("Choosing Friends to move in random manner");
-		System.out.println();
+		//System.out.println();
 		LocalSearcher L= new LocalSearcher(board,friends);
 		L.searchRand();
 		printGrid(L.board);
+		printFriends(L.board);
 		//this gets stuck
 		System.out.println();
 		System.out.println("Choosing Friends to move in min-conflict-first manner");
-		System.out.println();
+		//System.out.println();
 		///////////////////////
 		//randomlyInsertFriends(board2);
 		LocalSearcher L2=new LocalSearcher(board2,friends2);
 		L2.searchMin();
 		printGrid(L2.board);
+		printFriends(L2.board);
 		//this also gets stuck
 		System.out.println();
 		System.out.println("Choosing Friends to move in max-conflict-first manner");
-		System.out.println();
+		//System.out.println();
 		//randomlyInsertFriends(board3);
 		LocalSearcher L3=new LocalSearcher(board3,friends3);
 		L3.searchMax();
 		printGrid(L3.board);
+		printFriends(L3.board);
 		
 		
 	}
@@ -93,5 +97,13 @@ public class Runner {
 			returnable[i]=new forestSquare(orig[i].row,orig[i].col,orig[i].value);
 		}
 		return returnable;
+	}
+	public static void printFriends(forestSquare[][] board){
+		for(int i=0;i<board.length;i++){
+			for(int j=0;j<board[0].length;j++){
+				if(board[i][j].value=='F')
+					System.out.println((i+1)+" "+(j+1));
+			}
+		}
 	}
 }
